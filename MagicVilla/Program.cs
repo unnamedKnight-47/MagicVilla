f82d779fb39ee8c508a9e72c35f7a8ba0c4d3908
unnamedKnight <atikeprofessional117@gmail.com>
2025-08-12 23:50:45 +0600

    Configure Swagger

--- MagicVilla/Program.cs
+++ MagicVilla/Program.cs
@@ -3,15 +3,28 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+/***** Disabling OpenApi for Swagger
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+***************/
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    /* Disabling OpenApi
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        // Understand this line
+        options.SwaggerEndpoint("/openapi/v1.json", "VillaApi");
+    });
+    */
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
