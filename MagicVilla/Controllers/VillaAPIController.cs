3fcdbe3388e25c93858cc32c546abfa039cec5c7
unnamedKnight <atikeprofessional117@gmail.com>
2025-08-17 17:20:32 +0600

    Add GetVillas Endpoint

--- MagicVilla/Controllers/VillaAPIController.cs
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -1,9 +1,22 @@
+using MagicVilla.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla.Controllers
 {
+    // Specifies the URL to access this controller
+    [Route("/VillaApi")]
     [ApiController]
-    public class VillaAPIController: ControllerBase
+    public class VillaAPIController : ControllerBase
     {
+        // Specifies the HTTP Verb for the following action
+        [HttpGet]
+        public IEnumerable<VillaModel> GetVillas()
+        {
+            return new List<VillaModel>
+            {
+                new VillaModel { Id = 1, Name = "Pool View" },
+                new VillaModel { Id = 2, Name = "Beach View" },
+            };
+        }
     }
 }
